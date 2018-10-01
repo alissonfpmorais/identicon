5c5d8899e1ab432fc9ce0c4a37ea6b9cde28bf1d
Alisson Morais <alissonfpmorais@gmail.com>
2018-10-01 16:20:30 -0300

    add main & hash_string

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,25 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+    Generates an identicon based on a string
   """
+
+  def main(input) do
+    input
+    |> hash_string
+  end
 
   @doc """
-  Hello world.
+    Returns a list of integers representing hash.
+    The `input` is a string to calculate hash.
 
-  ## Examples
+  ## Example
 
-      iex> Identicon.hello()
-      :world
+      iex> Identicon.hash_string("identicon")
+      [173, 43, 65, 97, 60, 135, 2, 181, 55, 43, 189, 201, 168, 16, 112, 64]
 
   """
-  def hello do
-    :world
+  def hash_string(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
